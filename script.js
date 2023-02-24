dca7f856685df5472dcfe5c5ab09e4e012543a7f
Maëlys Deschaux <maelys.dcb@gmail.com>
2023-02-24 16:36:12 +0100

    Renaming classes in SCSS and fetching parameters and display it - JS in progress

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let city = document.querySelector(".localisation h2");
+let temp = document.querySelector(".localisation p");
+let weather = document.querySelector(".localisation button");
+let humidity = document.querySelector(".humidity p");
+let pressure = document.querySelector(".pressure p");
+let wind = document.querySelector(".wind p");
+let sunrise = document.querySelector(".sun__rise");
+let sunset = document.querySelector(".sun__set");
 // tableau des icones de météo
 let imgArrayDay = [
-    'images/sun.png',
-    'images/cloud.png',
-    'images/cloudy.png',
-    'images/rainy.png',
-    'images/storm.png',
-    'images/snowy.png',
-  ];
-
+  "images/sun.png",
+  "images/cloud.png",
+  "images/cloudy.png",
+  "images/rainy.png",
+  "images/storm.png",
+  "images/snowy.png",
+];
 
 // Alerte si la géolocalisation n'est pas activée
 if (navigator.geolocation) {
@@ -37,5 +43,16 @@ async function getWeatherData(long, lat) {
   console.log(data);
 
   city.innerHTML = `${data.city.name}`;
+  temp.innerHTML = Math.floor(`${data.list[0].main.temp}`) + "&deg;";
+  weather.innerHTML = `${data.list[0].weather[0].main}`;
+  humidity.innerHTML = `${data.list[0].main.humidity} %`;
+  pressure.innerHTML = `${data.list[0].main.pressure} mBar`;
+  wind.innerHTML = `${data.list[0].wind.speed} km`;
+
+  let unixTime = `${data.city.sunrise}` || `${data.city.sunset}`;
+  let date = new Date(unixTime * 1000);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  sunrise.innerHTML = hours + ":" + minutes.toLocaleString();
+  sunset.innerHTML = hours + ":" + minutes.toLocaleString();
 }
-  
